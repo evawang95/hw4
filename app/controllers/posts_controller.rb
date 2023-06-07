772c7c3b764cc635b7766d07b1eed28630567765
Zhiye Wang <eva.wang@kellogg.northwestern.edu>
2023-06-07 02:33:11 +0000

    ok

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,26 +6,17 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new
-    @post["title"] = params["post"]["title"]
-    @post["description"] = params["post"]["description"]
-    @post["posted_on"] = params["post"]["posted_on"]
-    @post["place_id"] = params["post"]["place_id"]
-    @post.save
-    redirect_to "/places/#{@post["place_id"]}"
-  end
-  
-  def update
-    @post = Post.find_by({ "id" => params["id"] })
     if @current_user
-      @company["name"] = params["company"]["name"]
-      @company["city"] = params["company"]["city"]
-      @company["state"] = params["company"]["state"]
-      @company.save
+      @post = Post.new
+      @post["title"] = params["post"]["title"]
+      @post["description"] = params["post"]["description"]
+      @post["posted_on"] = params["post"]["posted_on"]
+      @post["place_id"] = params["post"]["place_id"]
+      @post["user_id"] = @current_user["id"]
+      @post.save
+      redirect_to "/places/#{@post["place_id"]}"
     else
       flash["notice"] = "You must be logged in."
-    end
-    redirect_to "/companies/#{@company["id"]}"
   end
  
 end
